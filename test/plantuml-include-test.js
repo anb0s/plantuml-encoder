fe0085ec91893a8e645024336b50d5ea952d290c
Adrian Kosmaczewski <adrian@vshn.ch>
2019-10-25 13:40:27 +0200

    Added tests

--- test/plantuml-include-test.js
+++ test/plantuml-include-test.js
@@ -8,27 +8,34 @@ const minifiedDecoder = require('../dist/plantuml-decoder.min')
 
 const expect = chai.expect
 
-const uml = 'A -> B: Hello/你好'
-const encoded = 'SrJGjLDmibBmICt9oTT_idV1qwLx0G00'
+const umlWithInclude = `!include ./conf.plantuml
+
+Bob->Alice : hello`
+
+const umlWithoutInclude = `
+
+Bob->Alice : hello`
+
+const encoded = 'u-9ooa_IjNFCoKnELR1Io4ZDoSa70000'
 
 describe('plantuml-encoder', function () {
   describe('#encode()', function () {
-    it('node.js should encode UTF-8', function () {
-      const e = plantumlEncoder.encode(uml)
+    it('node.js should include file and encode UTF-8', function () {
+      const e = plantumlEncoder.encode(umlWithInclude)
       expect(e).to.equal(encoded)
     })
-    it('browser should encode UTF-8', function () {
-      expect(browserEncoder.encode(uml)).to.equal(plantumlEncoder.encode(uml))
+    it('browser should include file and encode UTF-8', function () {
+      expect(browserEncoder.encode(umlWithInclude)).to.equal(plantumlEncoder.encode(umlWithInclude))
     })
-    it('browser (minified) should encode UTF-8', function () {
-      expect(minifiedEncoder.encode(uml)).to.equal(plantumlEncoder.encode(uml))
+    it('browser (minified) should include file and encode UTF-8', function () {
+      expect(minifiedEncoder.encode(umlWithInclude)).to.equal(plantumlEncoder.encode(umlWithInclude))
     })
   })
 
   describe('#decode()', function () {
     it('node.js should decode UTF-8', function () {
       const decoded = plantumlEncoder.decode(encoded)
-      expect(decoded).to.equal(uml)
+      expect(decoded).to.equal(umlWithoutInclude)
     })
     it('browser should decode UTF-8', function () {
       expect(browserDecoder.decode(encoded)).to.equal(plantumlEncoder.decode(encoded))
